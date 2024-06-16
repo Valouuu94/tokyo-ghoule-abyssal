afa4a7a92ae07e8037477fa5366e12b2061cf7b7
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-06-16 21:44:44 +0200

    add layout and navbar

--- tokyo-ghoule-abyssal/src/app/app.component.ts
+++ tokyo-ghoule-abyssal/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainLayoutComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
