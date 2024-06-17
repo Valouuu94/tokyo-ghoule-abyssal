0df98c7bb656a127ba3c89b47d3f2c77a1600cd8
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-06-17 22:32:07 +0200

    add profils-rp component + font + routing

---
+++ tokyo-ghoule-abyssal/src/app/components/profils-rp/profils-rp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@Component({
+  selector: 'app-profils-rp',
+  standalone: true,
+  imports: [CheckboxModule],
+  templateUrl: './profils-rp.component.html',
+  styleUrl: './profils-rp.component.scss'
+})
+export class ProfilsRpComponent {
+
+}
