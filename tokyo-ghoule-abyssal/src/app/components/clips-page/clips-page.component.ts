6b556ddc7fa372d2d2e1e14742222a2c8cbe5972
HubertTruong <hubert.truong01@gmail.com>
2024-06-18 12:21:25 +0200

    ajout clips depuis firestore sur la page clips

--- tokyo-ghoule-abyssal/src/app/components/clips-page/clips-page.component.ts
+++ tokyo-ghoule-abyssal/src/app/components/clips-page/clips-page.component.ts
@@ -1,30 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { NgFor, NgIf } from '@angular/common';
-import { ClipsService } from '../../services/clips.service';
-import { Firestore } from '@angular/fire/firestore';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Clip } from '../../models/clip.model';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-clips-page',
   standalone: true,
-  imports: [NavBarComponent, NgFor, NgIf],
+  imports: [NavBarComponent, NgFor, NgIf, CommonModule],
   templateUrl: './clips-page.component.html',
   styleUrl: './clips-page.component.scss'
 })
 
-export class ClipsPageComponent implements OnInit {
-  mainClip: any = null;
-  otherClips: any[] = [];
+export class ClipsPageComponent {
+  clips$: Observable<any[]>;
 
-  constructor(private clipService: ClipsService, private db: Firestore) {}
-
-  ngOnInit(): void {
-    this.clipService.getData().subscribe(clips => {
-      if(clips.length > 0) {
-        this.mainClip = clips[0];
-        this.otherClips = clips.slice(1);
-      }
-    });
+  constructor(firestore: Firestore) {
+    const itemsCollection = collection(firestore, 'clips');
+    this.clips$ = collectionData(itemsCollection).pipe(
+      map(data => data as Clip[])
+    );
   }
-
 }
