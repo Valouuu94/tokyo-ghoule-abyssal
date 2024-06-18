b337dd3c085fdcfcdd2d601fdf1878b9ee53e7e9
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-06-18 23:22:22 +0200

    add final home page, css and image

--- tokyo-ghoule-abyssal/src/app/components/main-layout/main-layout.component.ts
+++ tokyo-ghoule-abyssal/src/app/components/main-layout/main-layout.component.ts
@@ -3,12 +3,13 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ClipsPageComponent } from "../clips-page/clips-page.component";
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
     selector: 'app-main-layout',
     standalone: true,
     templateUrl: './main-layout.component.html',
     styleUrl: './main-layout.component.scss',
-    imports: [NavBarComponent, ClipsPageComponent, NgIf]
+    imports: [NavBarComponent, ClipsPageComponent, NgIf, FooterComponent]
 })
 export class MainLayoutComponent {
     constructor(
