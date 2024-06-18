ed4eaadf353f9984b81b27f500d3d35606164ec6
HubertTruong <hubert.truong01@gmail.com>
2024-06-19 00:49:22 +0200

    meilleur design clips page et vues allClips et uniqueClip + ajout page add-clip

--- tokyo-ghoule-abyssal/src/app/app.routes.ts
+++ tokyo-ghoule-abyssal/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { ClipsPageComponent } from './components/clips-page/clips-page.component';
 import { ProfilsRpComponent } from './components/profils-rp/profils-rp.component';
+import { AddClipPageComponent } from './components/add-clip-page/add-clip-page.component';
 
 export const routes: Routes = [
   { path: 'admin', component: AdminComponent },
   { path: 'clips', component: ClipsPageComponent },
+  { path: 'clips/add-clip', component: AddClipPageComponent },
   { path: 'profils-rp', component: ProfilsRpComponent}
 
 ];
