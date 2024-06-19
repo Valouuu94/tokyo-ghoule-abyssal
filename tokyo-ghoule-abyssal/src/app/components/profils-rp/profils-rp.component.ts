be4f2451a792663a5d45408888fcb4dd90e13146
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-06-19 08:27:41 +0200

    add modif for profils page

--- tokyo-ghoule-abyssal/src/app/components/profils-rp/profils-rp.component.ts
+++ tokyo-ghoule-abyssal/src/app/components/profils-rp/profils-rp.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profils-rp',
   standalone: true,
-  imports: [CheckboxModule],
+  imports: [CheckboxModule, FormsModule],
   templateUrl: './profils-rp.component.html',
   styleUrl: './profils-rp.component.scss'
 })
 export class ProfilsRpComponent {
-
+    showCCG: boolean = false;
+    showGhoul: boolean = false;
+    showHuman: boolean = false;
 }
