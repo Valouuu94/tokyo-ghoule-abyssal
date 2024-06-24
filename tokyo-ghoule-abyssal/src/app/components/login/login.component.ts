ba54ee71536c217f6c7617893c7d3d19972cdf7d
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-06-24 13:56:05 +0200

    add style pour log discord

--- tokyo-ghoule-abyssal/src/app/components/login/login.component.ts
+++ tokyo-ghoule-abyssal/src/app/components/login/login.component.ts
@@ -8,50 +8,8 @@ import { DiscordIconComponent } from '../discord-icon/discord-icon.component';
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, DiscordIconComponent],
-  template: `
-    <div>
-      <div *ngIf="user; else notLoggedIn">
-        <h2>Welcome, {{ user.username }}</h2>
-        <img [src]="user.avatar ? 'https://cdn.discordapp.com/avatars/' + user.id + '/' + user.avatar + '.png' : 'default-avatar.png'" alt="User Avatar">
-        <button
-          class="discord-login"
-          (click)="logout()"
-        >
-          <span class="text-sm">Se Déconnecter</span>
-        </button>
-      </div>
-      <ng-template #notLoggedIn>
-        <button
-          class="discord-login"
-          (click)="login()"
-        >
-          <app-discord-icon></app-discord-icon>
-          <span class="text-sm">Se connecter avec Discord</span>
-        </button>
-      </ng-template>
-    </div>
-  `,
-  styles: [`
-    .discord-login {
-      display: flex;
-      align-items: center;
-      padding: 0.5rem 1rem;
-      border-radius: 0.5rem;
-      background-color: #5865f2;
-      color: white;
-      text-decoration: none;
-      border: none;
-      cursor: pointer;
-      transition: background-color 0.3s ease, color 0.3s ease;
-    }
-    .discord-login:hover {
-      background-color: #5865f2cc;
-      color: #fff8;
-    }
-    .text-sm {
-      margin-left: 1rem;
-    }
-  `],
+  templateUrl: `./login.component.html`,
+  styleUrls: [`./login.component.scss`]
 })
 export class LoginComponent implements OnInit {
   user: any;
